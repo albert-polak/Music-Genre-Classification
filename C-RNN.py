ace739263e3ecce604673ecf2ed1a9c7cb50624b
albert_polak <lugii1999@gmail.com>
2023-11-26 22:14:15 +0100

    working training model C-RNN

---
+++ C-RNN.py
@@ -0,0 +1,153 @@
+from torch.utils.data import DataLoader
+import os
+import torch
+import torch.nn as nn
+import pandas as pd
+import numpy as np
+
+from torchvision import models
+
+from torchvision import transforms
+
+from torchmetrics import Accuracy  
+
+import lightning as L
+from lightning.pytorch.tuner import Tuner
+
+from torch.optim import Adam
+
+import librosa
+
+from GTZAN_Dataset import GTZANDataModule
+
+class C_RNN(L.LightningModule):
+    def __init__(self, lr=1e-3, batch_size=1):
+        super().__init__()
+
+        self.lr = lr
+        self.batch_size = batch_size
+
+        self.num_classes = 10
+
+        self.cnn_model = nn.Sequential(
+            nn.Conv2d(1, 64, (3,3)),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, (3,3)),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, (3,3)),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, (3,3)),
+            nn.ReLU(),
+           
+        )
+
+        self.gru =  nn.GRU(83520, 256, 2, batch_first=True)
+        self.fc = torch.nn.Linear(256, 10)
+
+        self.loss_fn = nn.CrossEntropyLoss()  
+
+        self.acc = Accuracy('multiclass', num_classes=self.num_classes)
+
+        self.optimizer = Adam(self.parameters(), lr=self.lr)  
+
+    def forward(self, X):
+        X = X.to(torch.float32).cuda()
+        X = self.cnn_model(X)
+
+        X = X.view(X.size(0), X.size(1), X.size(2) * X.size(3))
+
+        print(X.shape)
+
+        h0 = torch.randn(2, self.batch_size, 256).cuda()
+
+        X, hn = self.gru(X, h0)
+
+        output = X[:, -1, :]
+
+        output = self.fc(output)
+
+        print(output.shape)
+
+        return output
+    
+    def configure_optimizers(self):
+            scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=30, gamma=0.90)
+            return {"optimizer": self.optimizer, "lr_scheduler": scheduler}
+
+    def _step(self, batch):
+        x, y = batch
+        preds = self(x)
+
+        # y = y.squeeze()  # Remove singleton dimensions
+        # print(y)
+        # print(preds)
+
+        loss = self.loss_fn(preds, y)
+        # acc = self.acc(preds, y)
+        acc_preds = torch.argmax(preds, dim=1)
+        acc = self.acc(acc_preds, y)
+        return loss, acc
+    
+    def training_step(self, batch, batch_idx):
+        loss, acc = self._step(batch)
+        # perform logging
+        self.log(
+            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
+        )
+        self.log(
+            "train_acc", acc, on_step=True, on_epoch=True, prog_bar=True, logger=True
+        )
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        loss, acc = self._step(batch)
+        # perform logging
+        self.log("val_loss", loss, on_epoch=True, prog_bar=False, logger=True)
+        self.log("val_acc", acc, on_epoch=True, prog_bar=True, logger=True)
+
+    def test_step(self, batch, batch_idx):
+        loss, acc = self._step(batch)
+        # perform logging
+        self.log("test_loss", loss, on_step=True, prog_bar=True, logger=True)
+        self.log("test_acc", acc, on_step=True, prog_bar=True, logger=True)
+
+
+
+if __name__ == "__main__":
+
+    rnn = C_RNN()
+    datamodule = GTZANDataModule('./data/genres_original/')
+
+    save_path = "./models"
+
+    checkpoint_callback = L.pytorch.callbacks.ModelCheckpoint(
+        dirpath=save_path,
+        filename="resnet-model-{epoch}-{val_loss:.2f}-{val_acc:0.2f}",
+        monitor="val_loss",
+        mode="min",
+        save_last=True,
+    )
+    trainer_args = {
+        "accelerator": "gpu",
+        "devices": "auto",
+        "strategy": "auto",
+        "max_epochs": 1000,
+        "callbacks": [checkpoint_callback],
+        "precision": 32,
+    }
+
+    trainer = L.Trainer(**trainer_args)
+
+    # duration = 30
+
+    # audio_sample, sampling_rate = librosa.load('./data/genres_original/blues/blues.00000.wav') 
+    # audio_sample, _ = librosa.effects.trim(audio_sample)
+
+    # mel = librosa.feature.melspectrogram(y=audio_sample, sr=sampling_rate)
+    # mel_db = librosa.amplitude_to_db(mel, ref=np.max)
+    # mel_db = mel_db[np.newaxis, ...]
+    # mel_db = torch.tensor(mel_db)
+
+    # rnn.forward(mel_db)
+
+    trainer.fit(model=rnn, datamodule=datamodule)
--- GTZAN_Dataset.py
+++ GTZAN_Dataset.py
@@ -3,7 +3,6 @@ from torch.utils.data import DataLoader
 import pandas as pd
 import librosa
 import numpy as np
-# Note - you must have torchvision installed for this example
 
 from torchvision import transforms
 
@@ -11,10 +10,10 @@ class GTZANDataModule(L.LightningDataModule):
     def __init__(self, data_dir: str = "./"):
         super().__init__()
         self.data_dir = data_dir
-        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
         self.sampling_rate = 12000
         self.duration = 30
         self.skip_duration = 15
+        self.batch_size = 1
         self.sample_duration = self.duration * self.sampling_rate
         self.sample_skip_duration = self.skip_duration * self.sampling_rate
 
@@ -27,7 +26,7 @@ class GTZANDataModule(L.LightningDataModule):
         audio_file = audio_file[self.sample_skip_duration:self.sample_duration+self.sample_skip_duration]
 
         if audio_file.shape[0] < self.sample_duration:
-            src = np.hstack((src, np.zeros((int(self.sample_duration) - audio_file.shape[0],))))
+            audio_file = np.hstack((audio_file, np.zeros((int(self.sample_duration) - audio_file.shape[0],))))
         
         mel = librosa.feature.melspectrogram(y=audio_file, sr=self.sampling_rate)
         mel_db = librosa.amplitude_to_db(mel, ref=np.max)
@@ -35,41 +34,43 @@ class GTZANDataModule(L.LightningDataModule):
 
     def setup(self, stage: str):
         # Assign train/val datasets for use in dataloaders
-        self.train = pd.read_csv('./train.csv')
-        self.test = pd.read_csv('./test.csv')
-        self.val = pd.read_csv('./val.csv')
-
+        self.train = pd.read_csv('./train.csv', sep=',')
+        self.test = pd.read_csv('./test.csv', sep=',')
+        self.val = pd.read_csv('./val.csv', sep=',')
         self.train_data = []
         self.test_data = []
         self.val_data = []
-        for i in self.train:
-            audio_file = librosa.load(self.data_dir+i[0], sr=self.sampling_rate)
+        for index, i in self.train.iterrows():
+            audio_file, _ = librosa.load(self.data_dir+i['label']+'/'+i['file'], sr=self.sampling_rate)
 
-            mel_db = self.to_mel(audio_file=audio_file)            
-
-            self.train_data.append([mel_db, i[1]])
-
-        for i in self.test:
-            audio_file = librosa.load(self.data_dir+i[0], sr=self.sampling_rate)
             mel_db = self.to_mel(audio_file=audio_file)
+            mel_db = mel_db[np.newaxis, ...]         
 
-            self.test_data.append([mel_db, i[1]])
+            self.train_data.append([mel_db, i['label_encoded']])
 
-        for i in self.val:
-            audio_file = librosa.load(self.data_dir+i[0], sr=self.sampling_rate)
+        for index, i in self.test.iterrows():
+            audio_file, _ = librosa.load(self.data_dir+i['label']+'/'+i['file'], sr=self.sampling_rate)
             mel_db = self.to_mel(audio_file=audio_file)
+            mel_db = mel_db[np.newaxis, ...]
 
-            self.val_data.append([mel_db, i[1]])
+            self.test_data.append([mel_db, i['label_encoded']])
+
+        for index, i in self.val.iterrows():
+            audio_file, _ = librosa.load(self.data_dir+i['label']+'/'+i['file'], sr=self.sampling_rate)
+            mel_db = self.to_mel(audio_file=audio_file)
+            mel_db = mel_db[np.newaxis, ...]
+
+            self.val_data.append([mel_db, i['label_encoded']])
 
 
     def train_dataloader(self):
-        return DataLoader(self.train_data, batch_size=32)
+        return DataLoader(self.train_data, batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self):
-        return DataLoader(self.val_data, batch_size=32)
+        return DataLoader(self.val_data, batch_size=self.batch_size, shuffle=True)
 
     def test_dataloader(self):
-        return DataLoader(self.test_data, batch_size=32)
+        return DataLoader(self.test_data, batch_size=self.batch_size, shuffle=True)
 
     # def predict_dataloader(self):
     #     return DataLoader(self.mnist_predict, batch_size=32)
